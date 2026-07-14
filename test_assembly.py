c1cff22ebb0a7ccea6db797fdeb4a608b6c7dbbc
giljung.yun <tornado1227@naver.com>
2026-07-14 09:52:01 +0900

    Initial commit

---
+++ test_assembly.py
@@ -0,0 +1,38 @@
+def test_delay():
+    assert False
+
+
+def test_clear():
+    assert False
+
+
+def test_show_menu():
+    assert False
+
+
+def test_is_valid_range():
+    assert False
+
+
+def test_select_car_type():
+    assert False
+
+
+def test_select_engine():
+    assert False
+
+
+def test_select_brake():
+    assert False
+
+
+def test_select_steering():
+    assert False
+
+
+def test_is_valid_check():
+    assert False
+
+
+def test_run_produced_car():
+    assert False
